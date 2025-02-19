5d8b51eb93a1ca393ea8f58f0fed1a82c8d85043
Jarrod Watts <jarrod@cubelabs.xyz>
2025-02-19 13:01:38 +1100

    Simple listening for chat messages in a stream

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+import { StreamChat, Event } from "stream-chat";
+import "dotenv/config";
+
+// Load environment variables (read .env.example for more info)
+const STREAM_API_KEY = process.env.STREAM_API_KEY!;
+const STREAM_AUTH_KEY = process.env.STREAM_AUTH_KEY!;
+const STREAMER_WALLET_ADDRESS = process.env.STREAMER_WALLET_ADDRESS!;
+const APP_WALLET_ADDRESS = process.env.APP_WALLET_ADDRESS!;
+
+(async () => {
+  const chatClient = StreamChat.getInstance(STREAM_API_KEY, {
+    allowServerSideConnect: true,
+  });
+
+  await chatClient.connectUser(
+    {
+      id: APP_WALLET_ADDRESS.toLowerCase(),
+    },
+    STREAM_AUTH_KEY
+  );
+
+  const channels = await chatClient.queryChannels({
+    created_by_id: STREAMER_WALLET_ADDRESS,
+  });
+
+  channels.forEach((channel) => {
+    channel.on("message.new", (event: Event) => {
+      console.log(event);
+    });
+  });
+
+  console.log(`Listening for messages...`);
+})();
