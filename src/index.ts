08e3973dfdb85abb1bfb7362e9ab8d5506c17e2c
Jarrod Watts <jarrod@cubelabs.xyz>
2025-03-07 17:33:48 +1100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { StreamChat, Event } from "stream-chat";
 import "dotenv/config";
-import { keyTap } from "robotjs";
 
 // Load environment variables (read .env.example for more info)
 const STREAM_API_KEY = process.env.STREAM_API_KEY!;
@@ -27,36 +26,8 @@ const APP_WALLET_ADDRESS = process.env.APP_WALLET_ADDRESS!;
   channels.forEach((channel) => {
     channel.on("message.new", (event: Event) => {
       if (!event.message?.text) return;
-      const command = event.message.text.toLowerCase();
 
-      console.log(command);
-
-      switch (command) {
-        case "up":
-          keyTap("up");
-          break;
-        case "down":
-          keyTap("down");
-          break;
-        case "left":
-          keyTap("left");
-          break;
-        case "right":
-          keyTap("right");
-          break;
-        case "a":
-          keyTap("x"); // assuming X is mapped to A button
-          break;
-        case "b":
-          keyTap("z"); // assuming Z is mapped to B button
-          break;
-        case "start":
-          keyTap("enter");
-          break;
-        case "select":
-          keyTap("backspace");
-          break;
-      }
+      console.log(`Received message: ${event.message.text}`);
     });
   });
 
